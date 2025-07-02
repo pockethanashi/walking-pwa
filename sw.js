fe507e5bfa70cfaca6024ba1a1a67ce2b31d80c0
pockethanashi <pocket.hanashi@gmail.com>
2025-07-02 19:56:06 +0900

    画面ついてるだけでノーカウント

--- sw.js
+++ sw.js
@@ -10,7 +10,6 @@ self.addEventListener('install', function (event) {
     })
   );
 });
-
 self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
